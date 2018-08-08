87e611de32ae4d2875a8e759511e6abcde18a61a
varshitha55 <41734732+varshitha55@users.noreply.github.com>
2018-08-08 11:28:58 +0530

    Create evenmedian.c

---
+++ evenmedian.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	// your code goes here
+	int i,a,b;
+	scanf("%d",&a);
+	scanf("%d",&b);
+	for(i=a;i<=b;i++)
+	{
+		if(i%2==0)
+		printf("%d ",i);
+	}
+}
+	
+	
