a4ac9662df61987da175a59f6e5a12c2e4264b5c
varshitha55 <41734732+varshitha55@users.noreply.github.com>
2018-08-09 11:21:42 +0530

    Update and rename knvalue.java to lcm.java

--- lcm.java
+++ lcm.java
@@ -5,17 +5,30 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class knvalue
+class lcm
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
 		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
-		int k=sc.nextInt();
-		int[] a=new int[n];
-		for(int i=0;i<n;i++)
-		a[i]=sc.nextInt();
-		System.out.println(a[--k]);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=lcm(a,b);
+		System.out.println(c);
+	}
+	static int lcm(int a,int b)
+	{
+		return a*b/gcd(a,b);
+	}
+	static int gcd(int a,int b)
+	{
+		if(a==0 ||b==0)
+			return 0;
+		if(a==b)
+			return a;
+		if(a>b)
+		return gcd(a-b,b);
+		return gcd(a,b-a);
 	}
 }
+	
