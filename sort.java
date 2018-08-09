7ba6e157e922650bae804d0aebf6d614f2de735c
varshitha55 <41734732+varshitha55@users.noreply.github.com>
2018-08-09 10:39:14 +0530

    Update and rename revnum.java to sort.java

--- sort.java
+++ sort.java
@@ -5,20 +5,15 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class revnum
+class sort
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
 		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
-		int rem=0,rev=0;
-		while(n>0)
-		{
-			rem=n%10;
-			rev=rev*10+rem;
-			n=n/10;
-		}
-		System.out.println(rev);
+		String str=sc.nextLine();
+		char c[]=str.toCharArray();
+		Arrays.sort(c);
+		System.out.println(c);
 	}
 }
