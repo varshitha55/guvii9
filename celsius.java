2c7463f48d4c13f0ed0f424c48ee68ebb7c96af5
varshitha55 <41734732+varshitha55@users.noreply.github.com>
2018-08-09 10:58:31 +0530

    Update and rename sumnum.java to celsius.java

--- celsius.java
+++ celsius.java
@@ -5,21 +5,14 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class sumnum
+class celsius
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
 		Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
-		int sum=0,i;
-		int[] a=new int[n];
-		for(i=0;i<n;i++)
-			a[i]=sc.nextInt();
-		for(i=0;i<n;i++)
-		{
-			sum +=a[i];
-		}
-		System.out.println(sum);
+		int celsius=n+273;
+		System.out.println(celsius);
 	}
 }
