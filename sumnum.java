93641eca5bc47ad71326c53a03a4b4487d2bf7fe
varshitha55 <41734732+varshitha55@users.noreply.github.com>
2018-08-09 10:52:49 +0530

    Update and rename sort.java to sumnum.java

--- sumnum.java
+++ sumnum.java
@@ -5,15 +5,21 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class sort
+class sumnum
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
 		Scanner sc=new Scanner(System.in);
-		String str=sc.nextLine();
-		char c[]=str.toCharArray();
-		Arrays.sort(c);
-		System.out.println(c);
+		int n=sc.nextInt();
+		int sum=0,i;
+		int[] a=new int[n];
+		for(i=0;i<n;i++)
+			a[i]=sc.nextInt();
+		for(i=0;i<n;i++)
+		{
+			sum +=a[i];
+		}
+		System.out.println(sum);
 	}
 }
