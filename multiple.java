81e1f6816896229ce81d8389f31f675910804ada
varshitha55 <41734732+varshitha55@users.noreply.github.com>
2018-08-09 12:17:40 +0530

    Update and rename ascii.java to multiple.java

--- multiple.java
+++ multiple.java
@@ -5,14 +5,17 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class ascii
+class multiple
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
 		Scanner sc=new Scanner(System.in);
-		char c=sc.next().charAt(0);
-		int a=c;
-		System.out.println(a);
+		int n=sc.nextInt();
+		if(n%13==0)
+			System.out.println("yes");
+		else 
+			System.out.println("no");
 	}
+	
 }
