5a3ef4301f49d1cd47809aa7e1aa875fa5e89d9c
varshitha55 <41734732+varshitha55@users.noreply.github.com>
2018-08-09 12:44:13 +0530

    Update and rename muldiv.java to secsmall.java

--- secsmall.java
+++ secsmall.java
@@ -5,17 +5,30 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class muldiv
+class secsmall
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
 		Scanner sc=new Scanner(System.in);
-		int a=sc.nextInt();
-		int b=sc.nextInt();
-		int c=sc.nextInt();
-		int pro=a*b;
-		int div=pro/c;
-		System.out.println(div);
+		int n=sc.nextInt();
+		int k=sc.nextInt();
+		int i=0;
+		int[] a=new int[n];
+		for(i=0;i<n;i++)
+		   a[i]=sc.nextInt();
+		for(i=0;i<n;i++)
+		{ 
+			for(int j=i+1;j<n;j++)
+			{
+				if(a[i]>a[j])
+				{
+					int temp=a[i];
+					a[i]=a[j];
+					a[j]=temp;
+				}
+			}
+		}
+		System.out.println(a[--k]);
 	}
 }
