2b9df8348c0aa3a9dc3d693a0a473b9bf3a1d96f
varshitha55 <41734732+varshitha55@users.noreply.github.com>
2018-08-09 10:32:59 +0530

    Update and rename factorial.java to revnum.java

--- revnum.java
+++ revnum.java
@@ -5,16 +5,20 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class factorial
+class revnum
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
 		Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
-		int fact=1;
-		for(int i=1;i<=n;i++)
-		fact *=i;
-		System.out.println(fact);
+		int rem=0,rev=0;
+		while(n>0)
+		{
+			rem=n%10;
+			rev=rev*10+rem;
+			n=n/10;
+		}
+		System.out.println(rev);
 	}
 }
