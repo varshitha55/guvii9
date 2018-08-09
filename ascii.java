ceda0793a4dd2eee9d9c921d0e79add52686bd36
varshitha55 <41734732+varshitha55@users.noreply.github.com>
2018-08-09 11:47:40 +0530

    Update and rename isogram.java to ascii.java

--- ascii.java
+++ ascii.java
@@ -5,24 +5,14 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class isogram
+class ascii
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
 		Scanner sc=new Scanner(System.in);
-		String str=sc.nextLine();
-		int count=0;
-		for(int i=0;i<str.length()-1;i++)
-		{
-			if(str.charAt(i)==str.charAt(i+1))
-			{
-				count++;
-			}
-		}
-		if(count==0)
-		System.out.println("Yes");
-		else 
-		System.out.println("No");
+		char c=sc.next().charAt(0);
+		int a=c;
+		System.out.println(a);
 	}
 }
