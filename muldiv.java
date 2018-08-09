1dcaa9c34106ce6ed4b857441e5267697443b187
varshitha55 <41734732+varshitha55@users.noreply.github.com>
2018-08-09 12:35:03 +0530

    Update and rename multiple.java to muldiv.java

--- muldiv.java
+++ muldiv.java
@@ -5,17 +5,17 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class multiple
+class muldiv
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
 		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
-		if(n%13==0)
-			System.out.println("yes");
-		else 
-			System.out.println("no");
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=sc.nextInt();
+		int pro=a*b;
+		int div=pro/c;
+		System.out.println(div);
 	}
-	
 }
