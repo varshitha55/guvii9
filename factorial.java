426aa2ce9da011c89341c64796e8ac5ff5d655be
varshitha55 <41734732+varshitha55@users.noreply.github.com>
2018-08-09 10:28:22 +0530

    Update and rename reverse.java to factorial.java

--- factorial.java
+++ factorial.java
@@ -5,19 +5,16 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class reverse
+class factorial
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
 		Scanner sc=new Scanner(System.in);
-		String str=sc.nextLine();
-		for(int i=str.length()-1;i>=0;i--)
-			{
-				System.out.print(str.charAt(i));
-			}
-			
-	
-	
+		int n=sc.nextInt();
+		int fact=1;
+		for(int i=1;i<=n;i++)
+		fact *=i;
+		System.out.println(fact);
 	}
 }
