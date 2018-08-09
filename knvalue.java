ef9a81ac4ea3ebe832da85cc95bd5032171d3f0f
varshitha55 <41734732+varshitha55@users.noreply.github.com>
2018-08-09 11:08:52 +0530

    Update and rename celsius.java to knvalue.java

--- knvalue.java
+++ knvalue.java
@@ -5,14 +5,17 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class celsius
+class knvalue
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
 		Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
-		int celsius=n+273;
-		System.out.println(celsius);
+		int k=sc.nextInt();
+		int[] a=new int[n];
+		for(int i=0;i<n;i++)
+		a[i]=sc.nextInt();
+		System.out.println(a[--k]);
 	}
 }
