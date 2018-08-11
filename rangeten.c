7c41bf8558440f36cfe51d46759f5fab44efcbb9
varshitha55 <41734732+varshitha55@users.noreply.github.com>
2018-08-11 08:31:30 +0530

    Create rangeten.c

---
+++ rangeten.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+	// your code goes here
+	int n;
+	scanf("%d",&n);
+	if(n>=1 &&n<=10)
+	{
+		printf("\n yes");
+	}
+	else 
+	{
+		printf("\n no");
+	}
+	return 0;
+}
