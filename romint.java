1400363f830f6a1102089c46550831a2ec783d80
varshitha55 <41734732+varshitha55@users.noreply.github.com>
2018-08-13 12:56:45 +0530

    Update and rename append.java to romint.java

---
+++ romint.java
@@ -0,0 +1,61 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		switch(str)
+		{
+			case "I":	System.out.println("1");
+				break;
+			case "II":	System.out.println("2");
+				break;
+			case "III":	System.out.println("3");
+				break;
+			case "IV":	System.out.println("4");
+				break;
+			case "V":	System.out.println("5");
+				break;
+			case "VI":	System.out.println("6");
+				break;
+			case "VII":	System.out.println("7");
+				break;
+		           case "VIII":	System.out.println("8");
+				break;
+			case "IX":	System.out.println("9");
+				break;
+			case "X":	System.out.println("10");
+				break;
+			case "XI":	System.out.println("11");
+				break;
+			case "XII":	System.out.println("12");
+				break;
+			case "XIII":	System.out.println("13");
+				break;
+			case "XIV":	System.out.println("14");
+				break;
+			case "XV":	System.out.println("15");
+				break;
+			case "XVI":	System.out.println("16");
+				break;
+			case "XVII":	System.out.println("17");
+				break;
+		           case "XVIII":System.out.println("18");
+				break;
+			case "XIX":	System.out.println("19");
+				break;
+			case "XX":	System.out.println("20");
+				break;
+				
+		}
+	}
+}
+		
