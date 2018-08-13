8d2a7b07ed60a530a04f1773fd9845e400538326
varshitha55 <41734732+varshitha55@users.noreply.github.com>
2018-08-13 10:42:27 +0530

    Update and rename join.java to min.java

--- min.java
+++ min.java
@@ -5,15 +5,30 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class join
+class min
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
 		Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
-		int p=sc.nextInt();
-		System.out.print(n);
-		System.out.print(p);
+		int[] a=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n-i-1;j++)
+			{
+				if(a[j]>a[j+1])
+				{
+					int temp=a[j];
+					a[j]=a[j+1];
+					a[j+1]=temp;
+				}
+			}
+		}
+		System.out.println(a[0]);
 	}
 }
