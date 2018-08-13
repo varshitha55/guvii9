e742ca195a2ec8a3aff6f21a9be8c15188468ead
varshitha55 <41734732+varshitha55@users.noreply.github.com>
2018-08-13 20:14:15 +0530

    Update numcount.c

--- numcount.c
+++ numcount.c
@@ -1,14 +1,16 @@
-#include<stdio.h>
-void main()
-{
-char str[50],i,c=0;
-gets(str);
-for(i=0;str[i]!='\0';i++)
-{
-if((str[i]>='0')&&(str[i]<='9'))
-{
-c++;
-}
-}
-printf("%d",c);
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	char str[50],i,c=0;
+	gets(str);
+	for(i=0;str[i]!='\0';i++)
+	{
+		if((str[i]>='0')&&(str[i]<='9'))
+		{
+			c++;
+		}
+	}
+	printf("%d",c);
+	return 0;
 }
