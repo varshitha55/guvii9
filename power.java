b8f7d36da26bf81cd49c5c0bab1cf2b0dcec6a75
varshitha55 <41734732+varshitha55@users.noreply.github.com>
2018-08-13 10:25:59 +0530

    Update and rename divtwo.java to power.java

--- power.java
+++ power.java
@@ -5,23 +5,25 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class divtwo
+class power
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
 		Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
-		int i=0;
-		while(n!=1)
+		int p=sc.nextInt();
+		int n1=n;
+		if(p==0)
+			n1=1;
+		else
 		{
-			if(n%2!=0)
-			{
-				i=n;
-				break;
-			}
-			n=n/2;
+		while(p>1)
+		{
+			n1=n1*n;
+			p--;
 		}
-		System.out.print(n);
+		}
+		System.out.println(n1);
 	}
 }
